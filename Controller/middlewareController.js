72d394b979152dd9fa68ce8d182e89ba3c4dd260
Le xuan chau <lexuanchau2001@gmail.com>
2023-04-19 19:23:13 +0700

    n

--- Controller/middlewareController.js
+++ Controller/middlewareController.js
@@ -5,7 +5,7 @@ const storageImages = multer.diskStorage({
     cb(null, "public/images/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + Math.round(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "." + file.originalname.split(".")[1]);
   },
 });
